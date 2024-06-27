5c333520dad4c9d813019f3f9e833cc3fc8b163d
anujyadav09 <anujy5952@gmail.com>
2024-06-27 19:47:08 +0530

    Add files via upload

---
+++ metacrafterassignment.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract anujyadav {
+
+    // Public variables
+    string public token_name = "anuj";
+    string public abbrev = "yadav";
+    uint public totalSupply;
+
+    // Mapping to store balances
+    mapping(address => uint) public balances;
+
+    // Mint function
+    function mint(address _to, uint _value) public {
+        totalSupply += _value;
+        balances[_to] += _value;
+    }
+
+    // Burn function
+    function burn(address _from, uint _value) public {
+        require(balances[_from] >= _value, "Insufficient balance to burn");
+        totalSupply -= _value;
+        balances[_from] -= _value;
+    }
+}
+      
